f81a3fd5069ab24827948db32ed42bd777446edb
Ajfury1 <ajfury1@gmail.com>
2024-12-27 09:41:10 +0530

    layout color change

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -10,13 +10,34 @@ import {
   } from "@/components/ui/sidebar"
 
 import {menuList} from '@/config/appSideBarList'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
   
 export function AppSidebar() {
+    const profileAvatar = '/images/developerAvatar.webp'
+
     return (
-      <Sidebar>
-        <SidebarContent>
+      <Sidebar collapsible="icon">
+        <SidebarContent className="bg-gray-800  text-gray-100">
           <SidebarGroup>
-            <SidebarGroupLabel>Application</SidebarGroupLabel>
+            {/* Profile section */}
+            <SidebarGroupLabel className="text-gray-100">
+              <div className="flex items-center gap-5">
+                <Avatar>
+                  <AvatarImage src={profileAvatar} />
+                  <AvatarFallback>AJ</AvatarFallback>
+                </Avatar>
+
+                <div className="flex flex-col items-start">
+                  <span className="text-sm">Ajithkumar</span>
+                  <span>ajith123@gmail.com</span>
+                </div>
+              </div>
+            </SidebarGroupLabel>
+            {/* divider section */}
+            <div className="border-b my-3"></div>
+            {/* content section */}
             <SidebarGroupContent>
               <SidebarMenu>
                 {menuList.map((item) => (
